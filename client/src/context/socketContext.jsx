c2156719deb38dba6f40d28671d87111f898c956
udit <raiudit80@gmail.com>
2024-04-15 21:19:26 +0530

    socket url updated

--- client/src/context/socketContext.jsx
+++ client/src/context/socketContext.jsx
@@ -14,7 +14,7 @@ export const SocketContextProvider = ({children})=>{
 
      useEffect(() => {
         if(user){
-            const socket = io("http://localhost:8000",{
+            const socket = io("https://talkitive.onrender.com/",{
                 query :{
                     userId :  user._id,
                 }
